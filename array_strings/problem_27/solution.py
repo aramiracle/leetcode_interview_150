9847fbfc5ff8a6f09e69e1da4f20260cd2405d22
aramiracle <a.r.amouzad.m@gmail.com>
2025-09-17 15:41:04 +0330

    solution of 27 with docstring

--- array_strings/problem_27/solution.py
+++ array_strings/problem_27/solution.py
@@ -1,6 +1,15 @@
 class Solution:
     def removeElement(self, nums, val):
         """
+        Removes all instances of `val` from the list `nums` in-place. 
+        The relative order of the remaining elements may change.
+
+        Args:
+            nums (List[int]): The input list of integers.
+            val (int): The value to remove from the list.
+
+        Returns:
+            int: The number of elements remaining in `nums` after removal.
         """
         k = 0
         for element in nums:
