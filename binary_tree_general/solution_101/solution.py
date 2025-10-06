82257408dfe27c4411783b76e34ac662bdab6a46
aramiracle <a.r.amouzad.m@gmail.com>
2025-10-06 23:30:04 +0330

    update solution of 101

--- binary_tree_general/solution_101/solution.py
+++ binary_tree_general/solution_101/solution.py
@@ -18,15 +18,15 @@ class Solution(object):
         if not root:
             return True
 
-        def isMirror(t1, t2):
+        def is_mirror(t1, t2):
             if not t1 and not t2:
                 return True
             if not t1 or not t2:
                 return False
             return (
                 t1.val == t2.val
-                and isMirror(t1.left, t2.right)
-                and isMirror(t1.right, t2.left)
+                and is_mirror(t1.left, t2.right)
+                and is_mirror(t1.right, t2.left)
             )
 
-        return isMirror(root.left, root.right)
+        return is_mirror(root.left, root.right)
