8cd5ce8afbd3fa3faae4596482e7d90ad4624c06
aramiracle <a.r.amouzad.m@gmail.com>
2025-10-06 23:25:42 +0330

    solution of 101

---
+++ binary_tree_general/solution_101/solution.py
@@ -0,0 +1,32 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        Determines whether a binary tree is symmetric around its center.
+
+        Args:
+            root (TreeNode | None): The root node of the binary tree.
+
+        Returns:
+            bool: True if the tree is symmetric, False otherwise.
+        """
+        if not root:
+            return True
+
+        def isMirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (
+                t1.val == t2.val
+                and isMirror(t1.left, t2.right)
+                and isMirror(t1.right, t2.left)
+            )
+
+        return isMirror(root.left, root.right)
